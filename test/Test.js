b8ccfc101eeabafcc8737e2039deff93f62c4e2e
babylonia.app <94221562+babyloniaapp@users.noreply.github.com>
2022-10-16 19:27:51 +0200

    Update Test.js

--- test/Test.js
+++ test/Test.js
@@ -6,7 +6,7 @@ describe("Token contract", function () {
     console.log(owner.address);
     const Token = await ethers.getContractFactory("Presale");
 
-    const presaleContract = await Token.deploy("0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee", "0x6d8787EA487Eb99633435E3ece87aD158c92D538", "0x49f86c1da7532280157227b2bd9f64fe6e250ef9");
+    const presaleContract = await Token.deploy("0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee", "0x6d8787EA487Eb99633435E3ece87aD158c92D538");
     // await presaleContract.connect(owner).DepositBusd(10);
     console.log(presaleContract.address);
   });
